66bf4049ca9c17e6dbaeb3ffd156ec7f7c6a7a98
JP-Norton <72099534+JP-Norton@users.noreply.github.com>
2024-12-17 21:28:51 -0500

    Initial Commit

---
+++ BuddyCheck/BuddyCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyCheckApp.swift
+//  BuddyCheck
+//
+//  Created by JP Norton on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BuddyCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuddyCheckTests/BuddyCheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyCheckTests.swift
+//  BuddyCheckTests
+//
+//  Created by JP Norton on 12/17/24.
+//
+
+import Testing
+@testable import BuddyCheck
+
+struct BuddyCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
